1a4f07c2874a36b9cffd3f8d81f728f7b8d0046e
yash0922 <yashkumarpal987@gmail.com>
2023-05-12 14:17:35 +0530

    update all

--- my-editor/src/Component/Editors.jsx
+++ my-editor/src/Component/Editors.jsx
@@ -1,99 +1,161 @@
 import React from 'react';
 import { useState,useRef } from 'react';
-import { Editor, EditorState,RichUtils } from 'draft-js';
+import { Editor, EditorState,RichUtils,Modifier } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 
 function Editors(props) {
     const [editorState, setEditorState] = useState(EditorState.createEmpty(),
       );
-      const [previousChar, setPreviousChar] = useState('');
-      const [previousCharTime, setPreviousCharTime] = useState(0);
-      const timeoutRef = useRef(null);
-
-    //   const handleBeforeInput = (chars, editorState) => {
-    //     console.log("Enter");
-    //     var timeoutID = setTimeout(() => {
-    //     if (chars === '*' ) {
-    //         console.log("Bold");
-           
-    //         setEditorState(RichUtils.toggleInlineStyle(editorState, 'BOLD'));
-       
-    //         return 'handled';
-    //       }
-    //     }, 1000);
-    //     clearTimeout(timeoutID)
-       
-
-    //     if (chars === '*' && editorState.getCurrentContent().getPlainText().endsWith('*'))  {
-    //       console.log("Color_red");
-          
-    //         setEditorState(RichUtils.toggleInlineStyle(editorState, 'RED_COLOR'));
-       
-       
-    //       return 'handled';
-    //     }
+     
+      // const currentContent = editorState.getCurrentContent();
+      // const plainText = currentContent.getPlainText();
+      // const selection = editorState.getSelection();
+      let count =0;
+      const handleBeforeInput = (chars, editorState) => {
+        console.log("Enter");
+      
     
-  
+        if(count<=1){
+          setTimeout(() => {
+         
+        
+            if (chars === '*' && editorState.getCurrentContent().getPlainText().startsWith('**'))  {
+              console.log("Underlined detected");
+              
+                setEditorState(RichUtils.toggleInlineStyle(editorState, 'UNDERLINE'));
+           
+           
+              return 'handled';
+            }
+             
+           else  if (chars === '*' && editorState.getCurrentContent().getPlainText().startsWith('*'))  {
+              console.log("Color_red");
+              
+                setEditorState(RichUtils.toggleInlineStyle(editorState, 'RED_COLOR'));
+           
+           
+              return 'handled';
+            }
+        
+            else if (chars === '*'  && editorState.getCurrentContent().getPlainText().endsWith('')) {
+                console.log("Bold");
+                setEditorState(RichUtils.toggleInlineStyle(editorState, 'BOLD'));
+                return 'handled';
+                
+              }
+            
+           
+            else if (chars === '#' && editorState.getCurrentContent().getPlainText().trim().endsWith(''))  {
+                    console.log("Enter Heading");
+                    setEditorState(RichUtils.toggleBlockType(editorState, 'header-one'));
+                    // Delay for 500ms
+                    return 'handled';
+                  }
+                  
+                 
+                  }, 1000);
+             count++;
+            }
+            else{
+              if (chars === '*' && editorState.getCurrentContent().getPlainText().startsWith('**'))  {
+                console.log("Underline character");
+                
+                  setEditorState(RichUtils.toggleInlineStyle(editorState, 'UNDERLINE'));
+             
+             
+                return 'handled';
+              }
+              else if (chars === '*' && editorState.getCurrentContent().getPlainText().startsWith('*'))  {
+                console.log("Color_red");
+                
+                  setEditorState(RichUtils.toggleInlineStyle(editorState, 'RED_COLOR'));
+             
+             
+                return 'handled';
+              }
+              else if (chars === '*'  && editorState.getCurrentContent().getPlainText().endsWith('')) {
+                  console.log("Bold");
+                  setEditorState(RichUtils.toggleInlineStyle(editorState, 'BOLD'));
+                  return 'handled';
+                  
+                }
+              
+             
+             
+      
+              
+              else if (chars === '#' && editorState.getCurrentContent().getPlainText().trim().endsWith(''))  {
+                      console.log("Enter Heading");
+                      setEditorState(RichUtils.toggleBlockType(editorState, 'header-one'));
+                      // Delay for 500ms
+                      return 'handled';
+                    }
+            }
+      }
+     
           
      
    
+    
+
+    // const handleBeforeInput = (chars, editorState) => {
+    //     const currentTime = Date.now();
+      
+    //     if (previousChar === '*' && chars === '*' && currentTime - previousCharTime < 700 ) {
+    //       clearTimeout(timeoutRef.current);
+    //       setEditorState(RichUtils.toggleInlineStyle(editorState, 'RED_COLOR'));
+    //       setPreviousChar('');
+    //       return 'handled';
+    //     } else if (previousChar === '' && chars === '*') {
+    //       setPreviousChar('*');
+    //       setPreviousCharTime(currentTime);
+    //       timeoutRef.current = setTimeout(() => {
+    //         setPreviousChar('');
+    //       }, 500);
+    //     } else if (previousChar === '*' && chars !== '*' && currentTime - previousCharTime < 700) {
+    //       setEditorState(RichUtils.toggleInlineStyle(editorState, 'BOLD'));
+    //       setPreviousChar('');
+    //       setPreviousCharTime(currentTime);
+    //       timeoutRef.current = setTimeout(() => {
+    //         setPreviousChar('');
+    //       }, 500);
+    //       return 'handled';
+    //     } else if (previousChar === '*' && chars === '*' && editorState.getCurrentContent().getPlainText().endsWith('**')) {
+    //         console.log("enter UnderLine")
+    //       setEditorState(RichUtils.toggleInlineStyle(editorState, 'UNDERLINE'));
+    //       setPreviousChar(chars);
+    //       setPreviousCharTime(currentTime);
+    //       timeoutRef.current = setTimeout(() => {
+    //         setPreviousChar('');
+    //       }, 500);
+    //       return 'handled';
+    //     } else if (previousChar === '*' && chars === '*' && editorState.getCurrentContent().getPlainText().endsWith('**')) {
+    //       setEditorState(RichUtils.toggleInlineStyle(editorState, 'RED_COLOR'));
+    //       setPreviousChar('');
+    //       return 'handled';
+    //     } else if (previousChar === '**' && chars === '*' && editorState.getCurrentContent().getPlainText().endsWith('*')) {
+    //       setEditorState(RichUtils.toggleInlineStyle(editorState, 'BOLD'));
+    //       setPreviousChar('');
+    //       return 'handled';
+    //     } else if (previousChar === '*' && chars === '*' && editorState.getCurrentContent().getPlainText().endsWith('***')) {
+    //       setEditorState(RichUtils.toggleInlineStyle(editorState, 'UNDERLINE'));
+    //       setPreviousChar('');
+    //       return 'handled';
+    //     } else if (chars === '#' && editorState.getCurrentContent().getPlainText().trim().endsWith(''))  {
+    //       console.log("Enter Heading");
+    //       setEditorState(RichUtils.toggleBlockType(editorState, 'header-one'));
+    //       // Delay for 500ms
+    //       return 'handled';
+    //     }
+      
+    //     setPreviousChar(chars);
+    //     setPreviousCharTime(currentTime);
     //     return 'not-handled';
     //   };
+    
+ 
 
-    const handleBeforeInput = (chars, editorState) => {
-        const currentTime = Date.now();
-      
-        if (previousChar === '*' && chars === '*' && currentTime - previousCharTime < 700) {
-          clearTimeout(timeoutRef.current);
-          setEditorState(RichUtils.toggleInlineStyle(editorState, 'RED_COLOR'));
-          setPreviousChar('');
-          return 'handled';
-        } else if (previousChar === '' && chars === '*') {
-          setPreviousChar('*');
-          setPreviousCharTime(currentTime);
-          timeoutRef.current = setTimeout(() => {
-            setPreviousChar('');
-          }, 500);
-        } else if (previousChar === '*' && chars !== '*' && currentTime - previousCharTime < 700) {
-          setEditorState(RichUtils.toggleInlineStyle(editorState, 'BOLD'));
-          setPreviousChar(chars);
-          setPreviousCharTime(currentTime);
-          timeoutRef.current = setTimeout(() => {
-            setPreviousChar('');
-          }, 500);
-          return 'handled';
-        } else if (previousChar === '*' && chars === '*' && currentTime - previousCharTime < 700) {
-            console.log("enter UnderLine")
-          setEditorState(RichUtils.toggleInlineStyle(editorState, 'UNDERLINE'));
-          setPreviousChar(chars);
-          setPreviousCharTime(currentTime);
-          timeoutRef.current = setTimeout(() => {
-            setPreviousChar('');
-          }, 500);
-        //   return 'handled';
-        // } else if (previousChar === '*' && chars === '*' && editorState.getCurrentContent().getPlainText().endsWith('**')) {
-        //   setEditorState(RichUtils.toggleInlineStyle(editorState, 'RED_COLOR'));
-        //   setPreviousChar('');
-        //   return 'handled';
-        // } else if (previousChar === '**' && chars === '*' && editorState.getCurrentContent().getPlainText().endsWith('*')) {
-        //   setEditorState(RichUtils.toggleInlineStyle(editorState, 'BOLD'));
-        //   setPreviousChar('');
-        //   return 'handled';
-        // } else if (previousChar === '*' && chars === '*' && editorState.getCurrentContent().getPlainText().endsWith('***')) {
-        //   setEditorState(RichUtils.toggleInlineStyle(editorState, 'UNDERLINE'));
-        //   setPreviousChar('');
-        //   return 'handled';
-        } else if (chars === '#' && editorState.getCurrentContent().getPlainText().trim().endsWith(''))  {
-          console.log("Enter Heading");
-          setEditorState(RichUtils.toggleBlockType(editorState, 'header-one'));
-          // Delay for 500ms
-          return 'handled';
-        }
-      
-        setPreviousChar(chars);
-        setPreviousCharTime(currentTime);
-        return 'not-handled';
-      };
+ 
       
       const customStyleMap = {
         RED_COLOR: {
@@ -122,6 +184,7 @@ function Editors(props) {
         editorState={editorState}
         handleBeforeInput={handleBeforeInput}
         onChange={setEditorState}
+        placeholder="Write something..."
         
       />
             
